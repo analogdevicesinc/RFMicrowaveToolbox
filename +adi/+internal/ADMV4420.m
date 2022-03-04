7b77a0a1c0b7e32e4b968d26862fa709da9e80f6
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2022-03-04 15:55:43 -0500

    Updated iioDevices property in ADMV4420 class
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- +adi/+internal/ADMV4420.m
+++ +adi/+internal/ADMV4420.m
@@ -36,7 +36,8 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         downCnvDeviceNames = {...
             'admv4420_CSB1',...
             'admv4420_CSB2'};
-    end    
+        downCnvDevices = {};
+    end
         
     methods
         %% Constructor
@@ -54,14 +55,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.LOFreqHz(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'lo_freq_hz',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'lo_freq_hz',obj.downCnvDevices{ii});
             end
         end
 
         function set.LOFreqHz(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'lo_freq_hz',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'lo_freq_hz',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -69,25 +70,16 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.MuxSel(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                if strcmp(...
-                        'LOW',...
-                        getDeviceAttributeRAW(obj,'mux_sel',128,obj.iioDevices{ii}))
+                tmp = getDeviceAttributeRAW(obj,'mux_sel',128,obj.downCnvDevices{ii});
+                if strcmp('LOW',tmp)
                     values(:,ii) = 0;
-                elseif strcmp(...
-                        'LOCK_DTCT',...
-                        getDeviceAttributeRAW(obj,'mux_sel',128,obj.iioDevices{ii}))
+                elseif strcmp('LOCK_DTCT',tmp)
                     values(:,ii) = 1;
-                elseif strcmp(...
-                        'R_COUNTER_PER_2',...
-                        getDeviceAttributeRAW(obj,'mux_sel',128,obj.iioDevices{ii}))
+                elseif strcmp('R_COUNTER_PER_2',tmp)
                     values(:,ii) = 2;
-                elseif strcmp(...
-                        'N_COUNTER_PER_2',...
-                        getDeviceAttributeRAW(obj,'mux_sel',128,obj.iioDevices{ii}))
+                elseif strcmp('N_COUNTER_PER_2',tmp)
                     values(:,ii) = 3;
-                elseif strcmp(...
-                        'HIGH',...
-                        getDeviceAttributeRAW(obj,'mux_sel',128,obj.iioDevices{ii}))
+                elseif strcmp('HIGH',tmp)
                     values(:,ii) = 4;
                 end
             end
@@ -97,16 +89,17 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
                     if (values(:,ii) == 0)
-                        setDeviceAttributeRAW(obj,'mux_sel','LOW',obj.iioDevices{ii});
+                        tmp = 'LOW';
                     elseif (values(:,ii) == 1)
-                        setDeviceAttributeRAW(obj,'mux_sel','LOCK_DTCT',obj.iioDevices{ii});
+                        tmp = 'LOCK_DTCT';
                     elseif (values(:,ii) == 2)
-                        setDeviceAttributeRAW(obj,'mux_sel','R_COUNTER_PER_2',obj.iioDevices{ii});
+                        tmp = 'R_COUNTER_PER_2';
                     elseif (values(:,ii) == 3)
-                        setDeviceAttributeRAW(obj,'mux_sel','N_COUNTER_PER_2',obj.iioDevices{ii});
+                        tmp = 'N_COUNTER_PER_2';
                     elseif (values(:,ii) == 4)
-                        setDeviceAttributeRAW(obj,'mux_sel','HIGH',obj.iioDevices{ii});
+                        tmp = 'HIGH';
                     end
+                    setDeviceAttributeRAW(obj,'mux_sel',tmp,obj.downCnvDevices{ii});
                 end
             end
         end
@@ -114,14 +107,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.NCounterFracVal(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'n_counter_frac_val',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'n_counter_frac_val',obj.downCnvDevices{ii});
             end
         end
 
         function set.NCounterFracVal(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'n_counter_frac_val',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'n_counter_frac_val',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -129,14 +122,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.NCounterIntVal(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'n_counter_int_val',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'n_counter_int_val',obj.downCnvDevices{ii});
             end
         end
 
         function set.NCounterIntVal(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'n_counter_int_val',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'n_counter_int_val',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -144,14 +137,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.NCounterModVal(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'n_counter_mod_val',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'n_counter_mod_val',obj.downCnvDevices{ii});
             end
         end
 
         function set.NCounterModVal(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'n_counter_mod_val',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'n_counter_mod_val',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -159,14 +152,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.PFDFreqHz(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'pfd_freq_hz',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'pfd_freq_hz',obj.downCnvDevices{ii});
             end
         end
 
         function set.PFDFreqHz(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'pfd_freq_hz',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'pfd_freq_hz',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -176,11 +169,11 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             for ii = 1:length(obj.downCnvDeviceNames)
                 if strcmp(...
                         'disabled',...
-                        getDeviceAttributeRAW(obj,'ref_divide_by_2',128,obj.iioDevices{ii}))
+                        getDeviceAttributeRAW(obj,'ref_divide_by_2',128,obj.downCnvDevices{ii}))
                     values(:,ii) = 0;
                 elseif strcmp(...
                         'enabled',...
-                        getDeviceAttributeRAW(obj,'ref_divide_by_2',128,obj.iioDevices{ii}))
+                        getDeviceAttributeRAW(obj,'ref_divide_by_2',128,obj.downCnvDevices{ii}))
                     values(:,ii) = 1;
                 end
             end
@@ -190,9 +183,9 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
                     if (values(:,ii) == 0)
-                        setDeviceAttributeRAW(obj,'ref_divide_by_2','disabled',obj.iioDevices{ii});
+                        setDeviceAttributeRAW(obj,'ref_divide_by_2','disabled',obj.downCnvDevices{ii});
                     elseif (values(:,ii) == 1)
-                        setDeviceAttributeRAW(obj,'ref_divide_by_2','enabled',obj.iioDevices{ii});
+                        setDeviceAttributeRAW(obj,'ref_divide_by_2','enabled',obj.downCnvDevices{ii});
                     end
                 end
             end
@@ -201,14 +194,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.RefDivider(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'ref_divider',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'ref_divider',obj.downCnvDevices{ii});
             end
         end
 
         function set.RefDivider(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'ref_divider',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'ref_divider',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -218,11 +211,11 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             for ii = 1:length(obj.downCnvDeviceNames)
                 if strcmp(...
                         'disabled',...
-                        getDeviceAttributeRAW(obj,'ref_doubler',128,obj.iioDevices{ii}))
+                        getDeviceAttributeRAW(obj,'ref_doubler',128,obj.downCnvDevices{ii}))
                     values(:,ii) = 0;
                 elseif strcmp(...
                         'enabled',...
-                        getDeviceAttributeRAW(obj,'ref_doubler',128,obj.iioDevices{ii}))
+                        getDeviceAttributeRAW(obj,'ref_doubler',128,obj.downCnvDevices{ii}))
                     values(:,ii) = 1;
                 end
             end
@@ -232,9 +225,9 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
                     if (values(:,ii) == 0)
-                        setDeviceAttributeRAW(obj,'ref_doubler','disabled',obj.iioDevices{ii});
+                        setDeviceAttributeRAW(obj,'ref_doubler','disabled',obj.downCnvDevices{ii});
                     elseif (values(:,ii) == 1)
-                        setDeviceAttributeRAW(obj,'ref_doubler','enabled',obj.iioDevices{ii});
+                        setDeviceAttributeRAW(obj,'ref_doubler','enabled',obj.downCnvDevices{ii});
                     end
                 end
             end
@@ -243,14 +236,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.RefFreqHz(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'ref_freq_hz',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'ref_freq_hz',obj.downCnvDevices{ii});
             end
         end
 
         function set.RefFreqHz(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'ref_freq_hz',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'ref_freq_hz',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -260,11 +253,11 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             for ii = 1:length(obj.downCnvDeviceNames)
                 if strcmp(...
                         'XTAL',...
-                        getDeviceAttributeRAW(obj,'ref_type',128,obj.iioDevices{ii}))
+                        getDeviceAttributeRAW(obj,'ref_type',128,obj.downCnvDevices{ii}))
                     values(:,ii) = 0;
 %                 elseif strcmp(...
 %                         'enabled',...
-%                         getDeviceAttributeRAW(obj,'ref_type',128,obj.iioDevices{ii}))
+%                         getDeviceAttributeRAW(obj,'ref_type',128,obj.downCnvDevices{ii}))
 %                     values(:,ii) = 1;
                 end
             end
@@ -274,9 +267,9 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
                     if (values(:,ii) == 0)
-                        setDeviceAttributeRAW(obj,'ref_type','XTAL',obj.iioDevices{ii});
+                        setDeviceAttributeRAW(obj,'ref_type','XTAL',obj.downCnvDevices{ii});
 %                     elseif (values(:,ii) == 1)
-%                         setDeviceAttributeRAW(obj,'ref_type','enabled',obj.iioDevices{ii});
+%                         setDeviceAttributeRAW(obj,'ref_type','enabled',obj.downCnvDevices{ii});
                     end
                 end
             end
@@ -285,14 +278,14 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
         function values = get.VCOFreqHz(obj)
             values = zeros(1, length(obj.downCnvDeviceNames));
             for ii = 1:length(obj.downCnvDeviceNames)
-                values(:,ii) = getDeviceAttributeLongLong(obj,'vco_freq_hz',obj.iioDevices{ii});
+                values(:,ii) = getDeviceAttributeLongLong(obj,'vco_freq_hz',obj.downCnvDevices{ii});
             end
         end
 
         function set.VCOFreqHz(obj, values)
             if obj.ConnectedToDevice
                 for ii = 1:length(obj.downCnvDeviceNames)
-                    setDeviceAttributeLongLong(obj,'vco_freq_hz',values(:,ii),obj.iioDevices{ii});
+                    setDeviceAttributeLongLong(obj,'vco_freq_hz',values(:,ii),obj.downCnvDevices{ii});
                 end
             end
         end
@@ -304,16 +297,16 @@ classdef (Abstract) ADMV4420 < adi.common.Attribute & ...
             % This is required sine Simulink support doesn't support
             % modification to nontunable variables at SetupImpl
             numDevs = obj.iio_context_get_devices_count(obj.iioCtx);
-            obj.iioDevices = cell(1,length(obj.downCnvDeviceNames));
+            obj.downCnvDevices = cell(1,length(obj.downCnvDeviceNames));
             for dn = 1:length(obj.downCnvDeviceNames)
                 for k = 1:numDevs
                     devPtr = obj.iio_context_get_device(obj.iioCtx, k-1);
                     name = obj.iio_device_get_name(devPtr);
                     if strcmpi(obj.downCnvDeviceNames{dn},name)
-                        obj.iioDevices{dn} = devPtr;
+                        obj.downCnvDevices{dn} = devPtr;
                     end
                 end
-                if isempty(obj.iioDevices{dn})
+                if isempty(obj.downCnvDevices{dn})
                    error('%s not found',obj.downCnvDeviceNames{dn});
                 end
             end
